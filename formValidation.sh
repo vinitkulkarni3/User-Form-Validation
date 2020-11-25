45d3dbae80964465a51ed03ce5e598420740acdc
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-25 08:25:26 +0530

    Added:First name validation

--- formValidation.sh
+++ formValidation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "WELCOME TO FORM VALIDATION USING REGULAR EXPRESSION"
+
+echo ""
+read -p "Enter first name: " fname
+pattern1="^[A-Z a-z]{3,}"
+if [[ $fname =~ $pattern1 ]]
+then
+  echo "$fname is valid"
+else
+  echo "$fname is not valid"
+fi
+echo "--------------------------------"
+echo ""
